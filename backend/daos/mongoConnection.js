9d4cf3a8a506d5f58550d2a00e23e77fa6bfbc47
Mark-VUW <zdy120939259@outlook.com>
2024-04-09 00:01:56 +1200

    Update mongoConnection.js

--- backend/daos/mongoConnection.js
+++ backend/daos/mongoConnection.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Config = require("config");
 
 async function connect() {
-  const address = Config.get('application.mongo.address');
-  const username = Config.get('application.mongo.username');
-  const password = Config.get('application.mongo.password');
+  // const address = Config.get('application.mongo.address');
+  // const username = Config.get('application.mongo.username');
+  // const password = Config.get('application.mongo.password');
   // const encodedPassword = encodeURIComponent(password); // Use encodeURIComponent to ensure special characters are correctly parsed
   
   // FIXME: specify the database name
-  await mongoose.connect(`mongodb://${username}:${password}@${address}/`, {
+  await mongoose.connect(`mongodb://devroot:devroot@mongo:27017/`, {
     // mongoose automatically does connection pooling and by default max pool size is 100.
     // We want this to be smaller to reduce load on server.
     // https://mongoosejs.com/docs/connections.html#connection_pools
