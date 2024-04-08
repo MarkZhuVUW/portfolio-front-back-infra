5559f8c9db59d42b50a3d34216708cc4efe47bc1
Mark-VUW <zdy120939259@outlook.com>
2024-04-09 00:41:22 +1200

    Update mongoConnection.js

--- backend/daos/mongoConnection.js
+++ backend/daos/mongoConnection.js
@@ -9,12 +9,12 @@ async function connect() {
 
   console.log(
     // eslint-disable-next-line no-undef
-    `mongodb://devroot:devroot@mongo:27017/`
+    `mongodb://devroot:devroot@${process.env.ME_CONFIG_MONGODB_SERVER}:27017/`
   );
   // FIXME: specify the database name
   await mongoose.connect(
     // eslint-disable-next-line no-undef
-    `mongodb://devroot:devroot@mongo:27017/`,
+    `mongodb://devroot:devroot@${process.env.ME_CONFIG_MONGODB_SERVER}:27017/`,
     {
       // mongoose automatically does connection pooling and by default max pool size is 100.
       // We want this to be smaller to reduce load on server.
