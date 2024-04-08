8fb8d6f3dbe4ca2df72fc8c1afb4e042dcd433d9
Mark-VUW <zdy120939259@outlook.com>
2024-04-09 00:08:07 +1200

    1

--- backend/daos/mongoConnection.js
+++ backend/daos/mongoConnection.js
@@ -8,7 +8,7 @@ async function connect() {
   // const encodedPassword = encodeURIComponent(password); // Use encodeURIComponent to ensure special characters are correctly parsed
   
   // FIXME: specify the database name
-  await mongoose.connect(`mongodb://devroot:devroot@mongo:27017/`, {
+  await mongoose.connect(`mongodb://devroot:devroot@${process.env.ME_CONFIG_MONGODB_SERVER}:27017/`, {
     // mongoose automatically does connection pooling and by default max pool size is 100.
     // We want this to be smaller to reduce load on server.
     // https://mongoosejs.com/docs/connections.html#connection_pools
