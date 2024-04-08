aeb0febcca476c6c0eef83dfdac00b1b0314a660
Mark-VUW <zdy120939259@outlook.com>
2024-04-08 16:55:04 +1200

    Update userService.js

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,5 +1,5 @@
 const userDao = require("../daos/userDao.js");
-const UserDto = require("../dtos/userDto.js");
+const UserDto = require("../dtos/userDto");
 
 async function createUser(userData) {
   const user = await userDao.createUser(userData);
